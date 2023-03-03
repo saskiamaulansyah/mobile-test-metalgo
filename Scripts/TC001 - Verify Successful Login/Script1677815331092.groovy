e8365dd51b941eb3b256e53da774a03c75bed58d
Saskia Maulansyah <Saskia Maulansyah@gmail.com>
2023-03-03 10:51:07 +0700

    Adding Success Login TC

---
+++ Scripts/TC001 - Verify Successful Login/Script1677815331092.groovy
@@ -0,0 +1,30 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('D:\\Downloads\\MetalGODebug-20230210.apk', true)
+
+Mobile.tap(findTestObject('Object Repository/Page_Login/android.widget.Button - Masuk Sekarang'), 0)
+
+Mobile.setText(findTestObject('Object Repository/Page_Login/android.widget.EditText - codenameich (1)'), 'saskiamaulansyah', 
+    0)
+
+Mobile.setText(findTestObject('Object Repository/Page_Login/android.widget.EditText -'), 'testing1', 0)
+
+Mobile.tap(findTestObject('Object Repository/Page_Login/android.widget.Button - Masuk Sekarang (1)'), 0)
+
