af99e447375780fbc3d90ce79898fff2a78b765a
Saskia Maulansyah <Saskia Maulansyah@gmail.com>
2023-03-07 16:55:51 +0700

    Add Login more than once TC

---
+++ Scripts/TC015 - Verify History Transactions/Script1678164741000.groovy
@@ -0,0 +1,53 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('D:\\Downloads\\MetalGODebug-20230306.apk', true)
+
+Mobile.setText(findTestObject('Object Repository/Page_TransactionHistory/android.widget.EditText - Masukkan username  email  no telp'), 
+    'saskiamaulansyah', 0)
+
+Mobile.setText(findTestObject('Object Repository/Page_TransactionHistory/android.widget.EditText - Masukkan password'), 
+    'testing1', 0)
+
+Mobile.tap(findTestObject('Object Repository/Page_TransactionHistory/android.widget.Button - Masuk Sekarang'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Page_TransactionHistory/android.widget.ImageView'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Page_TransactionHistory/android.widget.TextView - Selesai'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Page_TransactionHistory/android.widget.TextView - Berlangsung'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Page_TransactionHistory/android.widget.ImageView (1)'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Page_TransactionHistory/android.widget.EditText - Semua'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Page_TransactionHistory/android.widget.ImageView (2)'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Page_TransactionHistory/android.widget.Button - Terapkan'), 0)
+
+Mobile.tap(findTestObject(''), 0)
+
+Mobile.tap(findTestObject('Object Repository/Page_TransactionHistory/android.widget.ImageButton'), 0)
+
+Mobile.tap(findTestObject(''), 0)
+
+Mobile.tap(findTestObject('Object Repository/Page_TransactionHistory/android.widget.Button - Nanti Saja'), 0)
+
+Mobile.closeApplication()
+
